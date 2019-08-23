9e2508d8c7816d7b302e0c3d69ae395d6080c8ff
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-22 23:50:45 -0400

    Update CustomerOrder.sql

--- CustomerOrder.sql
+++ CustomerOrder.sql
@@ -1,13 +1,32 @@
-use empdb1;
-select client_name, city from empdb1.client;
+use music_db;
+select client_name, city from client;
 select * from product;
 select client_name from client where client_name like '_a%';
 select * from client where city like 'Tezpur';
-select * from empdb1.product;
-select * from empdb1.product where selling_price between 2001 AND 5000;
-select * from empdb1.order;
-ALTER TABLE `empdb1`.`product` 
+select * from product;
+select * from product where selling_price between 2001 AND 5000;
+
+ALTER TABLE `music_db`.`product` 
 ADD COLUMN `new_price` DECIMAL(10,2) NULL;
-alter TABLE `empdb1`.`order`
-rename column prod_rate to new_prod_rate;
+select * from product order by description;
+select order_date,idorder from music_db.order;
+
+select * from music_db.order;
+delete from music_db.order where order_date < '2008-08-25';
+
+UPDATE `music_db`.`order`
+SET
+`delivery_date`=16-08-08
+WHERE idorder='ON01008';
+
+UPDATE `music_db`.`client`
+SET
+`total_bal`=1200
+WHERE idclient= 'CN01003';
+
+select * from product where description like 'HDD1034' or 'DVDRW';
+
+select client_name,city,State from client where State <> 'Assam';
+
+select * from orders where cancel_date like '%-03-%';
 
